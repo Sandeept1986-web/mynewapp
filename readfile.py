081c4a650941b9b1fe1c4a2abc421399adfc3f0f
Sandeept1986-web <thontlasandeep28@gmail.com>
2026-01-31 12:53:06 +0530

    new

--- readfile.py
+++ readfile.py
@@ -1,54 +1,5 @@
-# Data types
-# 1 Int:
+with open("student_data.txt", "r") as file:
+    content = file.read()
 
-#num1 = 100
-#rint(type(num1))
-
-# 2 float:
-
-#num2 = 15.25
-#print(type(num2))
-
-#num1 = 100
-#print(id(num1))
-#num1 = 105
-#print(id(num1))
-
-# 3 str '' "" """ """
-
-#s = "'Python' sample string"
-#print(id(s))
-#print(s,(type(s)))
-
-
-#s = "new value"
-#print(id(s))
-
-# 4 list : []
-
-# a = [10,20,30,40,50,"Python", "Django"]
-# print(id(a))
-# a.append(60)
-# print(id(a))
-# print(a)
-
-
-# 5 tuple :
-
-# t = (10,20,30)
-
-# 6 Dict :
-
-# d = {"name":"ABC","email":"abc@gmail.com"}
-  
-# 7 Set {}
-# s = {10,20,30,40}
-
-# 8 bool : True False
-
-# 9 complex : 4 + 3j
-
-help(str)
-
-
-
+print("File Content:\n")
+print(content)
